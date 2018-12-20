1b98089716a45f547af62f5351213ab50186a006
eoller <Egorpyp@gmail.com>
2018-12-20 16:18:22 +0100

    init

---
+++ src/main/java/pl/eoller/cryptonit/CryptonitApplication.java
@@ -0,0 +1,14 @@
+package pl.eoller.cryptonit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptonitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptonitApplication.class, args);
+	}
+
+}
+
